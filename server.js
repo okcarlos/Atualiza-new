9a95ea034c2410dffa5777a4c31945f0ee7c8107
okcarlos <okcarlos122@gmail.com>
2026-09-24 09:17:49 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 const express = require("express");
@@ -23,19 +22,19 @@ const NEW = {
     IEV: {
 
         TOKEN:
-            process.env.IEV_NEWCORBAN_TOKEN,
+            process.env.NEWCORBAN_IEV_TOKEN,
 
         BASE_URL:
-            process.env.IEV_NEWCORBAN_BASE_URL
+            process.env.NEWCORBAN_IEV_BASE_URL
     },
 
     CS: {
 
         TOKEN:
-            process.env.CS_NEWCORBAN_TOKEN,
+            process.env.NEWCORBAN_CS_TOKEN,
 
         BASE_URL:
-            process.env.CS_NEWCORBAN_BASE_URL
+            process.env.NEWCORBAN_CS_BASE_URL
     }
 };
 
